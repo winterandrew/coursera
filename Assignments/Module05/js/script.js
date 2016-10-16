1378f25a6cae08a8f3c7168f4f506e839ed1d964
Andrew Winter <awinter@startgroup.com.au>
2016-10-17 09:03:34 +1100

    Type in contect selector

--- Assignments/Module05/js/script.js
+++ Assignments/Module05/js/script.js
@@ -83,7 +83,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
- // [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
   buildAndShowHomeHTML,
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
@@ -128,7 +127,7 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-       insertHtml("#ain-content", homeHtmlToInsertIntoMainPage);
+       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
